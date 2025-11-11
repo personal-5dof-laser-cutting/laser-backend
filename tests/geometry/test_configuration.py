783253e5f2892469f7ea45710b34141e4305f5ad
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2025-11-11 15:59:52 +0100

    parameterise config tests

--- tests/geometry/test_configuration.py
+++ tests/geometry/test_configuration.py
@@ -4,54 +4,28 @@ import pytest
 import math
 
 
-def test_straight_config():
-    seg = Segment(Point(0, 0, 1), Point(0, 0, 0))
-    config = Configuration.from_segment(seg)
-
-    assert config.x == 0
-    assert config.y == 0
-    assert config.alpha_deg == 0.0
-    assert config.beta_deg == 0.0
-
-
-def test_angled_config_x():
-    seg = Segment(Point(0, 0, 1), Point(1, 0, 0))
-    config = Configuration.from_segment(seg)
-
-    assert config.x == 0
-    assert config.y == 0
-    assert config.alpha_deg == 0
-    assert config.beta_deg == pytest.approx(45, 0.01)
-
-
-def test_angled_config_y():
-    seg = Segment(Point(0, 0, 1), Point(0, 1, 0))
-    config = Configuration.from_segment(seg)
-
-    assert config.x == 0
-    assert config.y == 0
-    assert config.alpha_deg == pytest.approx(45, 0.01)
-    assert config.beta_deg == 0
-
-
-def test_angled_config1():
-    seg = Segment(Point(1.5, 1, 1), Point(0, 0, 0))
-    config = Configuration.from_segment(seg)
-
-    assert config.x == 1.5
-    assert config.y == 1
-    assert config.alpha_deg == pytest.approx(math.degrees(math.atan(-1)), 0.01)
-    assert config.beta_deg == pytest.approx(math.degrees(math.atan(-1.5)), 0.01)
-
-
-def test_angled_config2():
-    seg = Segment(Point(0, 0, 1), Point(-1, -0.5, 0))
-    config = Configuration.from_segment(seg)
-
-    assert config.x == 0
-    assert config.y == 0
-    assert config.beta_deg == pytest.approx(-45.0, 0.01)
-    assert config.alpha_deg == pytest.approx(-math.degrees(math.atan(0.5)), 0.01)
+@pytest.mark.parametrize(
+    "segment, expected_x, expected_y, expected_alpha, expected_beta",
+    [
+        (Segment(Point(0, 0, 1), Point(0, 0, 0)), 0, 0, 0, 0),
+        (Segment(Point(0, 0, 1), Point(1, 0, 0)), 0, 0, 0, 45),
+        (Segment(Point(0, 0, 1), Point(0, 1, 0)), 0, 0, 45, 0),
+        (Segment(Point(1.5, 1, 1), Point(0, 0, 0)), 1.5, 1, -45, -56.31),
+        (Segment(Point(0, 0, 1), Point(-1, -0.5, 0)), 0, 0, -26.56, -45),
+    ],
+)
+def test_config(
+    segment: Segment,
+    expected_x: float,
+    expected_y: float,
+    expected_alpha: float,
+    expected_beta: float,
+):
+    config = Configuration.from_segment(segment)
+    assert config.x == pytest.approx(expected_x, 0.01)
+    assert config.y == pytest.approx(expected_y, 0.01)
+    assert config.alpha_deg == pytest.approx(expected_alpha, 0.01)
+    assert config.beta_deg == pytest.approx(expected_beta, 0.01)
 
 
 def test_deg_rad_conversion():
