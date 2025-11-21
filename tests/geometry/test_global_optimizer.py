de8bfc3a72cac59c6107bc99c0dfe9ca68fd4b27
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2025-11-21 15:51:18 +0100

    write and improve testing for global optimizer

--- tests/geometry/test_global_optimizer.py
+++ tests/geometry/test_global_optimizer.py
@@ -1,8 +1,14 @@
 import pytest
+import networkx as nx
 from Geometry3D import Point
 from core.pipeline.global_optimizer import GlobalOptimizerModule
 from core.models.geometry import Geometry, TrapezoidalCut, Configuration
-from core.services.cost_function_service import CostFunctionService
+from core.service_container import Container
+from core.services.cost_function_service import (
+    CostFunctionService,
+    CostFunctionServiceImpl,
+)
+from dependency_injector.wiring import inject, Provide
 
 TEST_PARAMS = [
     [
@@ -22,6 +28,45 @@ TEST_PARAMS = [
 ]
 
 
+@pytest.mark.parametrize(
+    "cut1, cut2",
+    [
+        (
+            TrapezoidalCut(
+                Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
+            ),
+            TrapezoidalCut(
+                Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
+            ),
+        )
+    ],
+)
+def test_build_graph(cut1: TrapezoidalCut, cut2: TrapezoidalCut):
+    generated_graph = nx.Graph()
+    control_graph = nx.Graph()
+    cost_function = CostFunctionServiceImpl()
+    optimizer = GlobalOptimizerModule()
+    optimizer._build_graph(generated_graph, [cut1, cut2], cost_function)
+
+    expected_nodes = [
+        cut1,
+        cut1.start_configuration(),
+        cut1.end_configuration(),
+        cut2,
+        cut2.start_configuration(),
+        cut2.end_configuration(),
+    ]
+    control_graph.add_nodes_from(expected_nodes)
+    assert control_graph.nodes == generated_graph.nodes
+
+    for u in expected_nodes:
+        for v in expected_nodes:
+            if u != v:
+                control_graph.add_edge(u, v)
+
+    assert control_graph.edges == generated_graph.edges
+
+
 @pytest.mark.parametrize("cuts", TEST_PARAMS)
 def test_path_to_trapezoids(
     cuts: list[TrapezoidalCut],
@@ -35,20 +80,17 @@ def test_path_to_trapezoids(
 
     go = GlobalOptimizerModule()
     trapezoids = go._path_to_trapezoids(path)
-    for trapezoid, cut in zip(trapezoids, cuts):
-        assert trapezoid == cut
+    assert set(trapezoids) == set(cuts)
 
     ls_path = path
     ls_path.append(ls_path.pop(0))
     trapezoids = go._path_to_trapezoids(path)
-    for trapezoid, cut in zip(trapezoids, cuts):
-        assert trapezoid == cut
+    assert set(trapezoids) == set(cuts)
 
     rs_path = path
     rs_path.insert(0, rs_path.pop())
     trapezoids = go._path_to_trapezoids(path)
-    for trapezoid, cut in zip(trapezoids, cuts):
-        assert trapezoid == cut
+    assert set(trapezoids) == set(cuts)
 
 
 @pytest.mark.parametrize("cuts", TEST_PARAMS)
@@ -64,11 +106,17 @@ def test_global_optimizer(cuts: list[TrapezoidalCut]):
     assert original_costs > optimized_costs or original_costs == pytest.approx(
         optimized_costs
     )
+    print(f"Original cost was {original_costs}")
+    print(f"Optimized cost is {optimized_costs}")
 
 
-def _calculate_path_cost(path: list[TrapezoidalCut]):
+@inject
+def _calculate_path_cost(
+    path: list[TrapezoidalCut],
+    cost_function: CostFunctionService = Provide[Container.cost_function],
+):
     total_cost = 0
-    cost_function = CostFunctionService()
+    cost_function = CostFunctionServiceImpl()
     for i in range(len(path) - 1):
         total_cost += cost_function.get_cost(
             path[i].end_configuration(), path[i + 1].start_configuration()
