53b2cab9afdc8393e44a45532e86b78c26327a74
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2025-11-25 14:40:22 +0100

    add gcode export tests

---
+++ tests/gcode_export/test_gcode_export.py
@@ -0,0 +1,10 @@
+from core.modules.gcode_exporter.gcode_export import GCodeExporter
+from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
+
+
+def test_gcode_export():
+    exporter = GCodeExporter()
+    importer = SVG5DOF_Importer()
+    geo = importer.process((open("tests/svg5dof/svgs/one_line.svg").read(), 5.0))
+    gcode = exporter.process((geo, 5.0))
+    print(gcode)
