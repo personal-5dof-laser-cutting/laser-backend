909b31913adbb49b75c1ae72fc5ebd5525fa5dc9
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2025-12-01 13:19:44 +0100

    add optional comments to gcode & fix -0.0

--- core/modules/gcode_exporter/gcode_export.py
+++ core/modules/gcode_exporter/gcode_export.py
@@ -6,9 +6,13 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
     def __init__(self) -> None:
         super().__init__()
         self.gcode: str = ""
+        self.add_comments: bool = True
 
-    def _add_command(self, command: str):
-        self.gcode += f"{command}\n"
+    def _add_command(self, command: str, comment: str = ""):
+        self.gcode += f"{command}"
+        if self.add_comments and comment != "":
+            self.gcode += f"; {comment}"
+        self.gcode += "\n"
 
     def process(self, data: tuple[Geometry, float]) -> str:
         geometry, material_height = data
@@ -16,10 +20,10 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
         cut_speed: float = 10  # mm/s
         material_constant: float = 3.21  # s/mm^2
 
-        self._add_command("G90")  # absolute positioning
-        self._add_command("G21")  # use milimeters for XYZ
-        self._add_command("F1000")  # set feedrate
-        self._add_command("M8")  # air assist on (flood pin)
+        self._add_command("G90", "absolute positioning")  # absolute positioning
+        self._add_command("G21", "units in mm")  # use milimeters for XYZ
+        self._add_command("F1000", "feedrate")  # set feedrate
+        self._add_command("M8", "air assist on")  # air assist on (flood pin)
 
         last_config: None | Configuration = None
         for cut in geometry.cuts:
@@ -27,19 +31,21 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
 
             if last_config is not None and last_config != start_config:
                 self._add_command(
-                    f"G0 {start_config.x} {start_config.y} {material_height} {start_config.alpha} {start_config.beta}"
+                    f"G0 {start_config.x} {start_config.y} {material_height} {start_config.alpha} {start_config.beta + 0.0}",
+                    "travel move",
                 )  # travel move
 
             end_config = cut.end_configuration()
             self._add_command(
-                f"M4 S{cut.cut_depth * cut_speed * material_constant}"
+                f"M4 S{cut.cut_depth * cut_speed * material_constant}", "laser on"
             )  # laser on dynamic power
             self._add_command(
-                f"G1 {end_config.x} {end_config.y} {material_height} {end_config.alpha} {end_config.beta}"
+                f"G1 {end_config.x} {end_config.y} {material_height} {end_config.alpha + 0.0} {end_config.beta + 0.0}",
+                "cut move",
             )  # cut
-            self._add_command("M4 S0")  # laser off
+            self._add_command("M4 S0", "laser off")  # laser off
             last_config = end_config
 
-        self._add_command("M8.1")  # air assist off
-        self._add_command("M2")  # end of program
+        self._add_command("M8.1", "air assist off")  # air assist off
+        self._add_command("M2", "end")  # end of program
         return self.gcode
