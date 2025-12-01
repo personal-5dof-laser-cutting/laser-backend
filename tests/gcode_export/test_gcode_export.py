5087780673fe6453bac98a417e8a17559debf88b
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2025-12-01 14:47:51 +0100

    debug testing

--- tests/gcode_export/test_gcode_export.py
+++ tests/gcode_export/test_gcode_export.py
@@ -5,6 +5,7 @@ from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
 def test_gcode_export():
     exporter = GCodeExporter()
     importer = SVG5DOF_Importer()
-    geo = importer.process((open("tests/svg5dof/svgs/one_line.svg").read(), 5.0))
+    geo = importer.process((open("tests/svg5dof/svgs/circle.svg").read(), 5.0))
     gcode = exporter.process((geo, 5.0))
-    print(gcode)
+    with open("output.gcode", "w+") as f:
+        f.write(gcode)
