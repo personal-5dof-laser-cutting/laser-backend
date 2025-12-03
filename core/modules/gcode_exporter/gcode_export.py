98f305334cf26133b0d20358f594147889bdc821
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2025-12-03 14:37:07 +0100

    add config options & laser power calculations

--- core/modules/gcode_exporter/gcode_export.py
+++ core/modules/gcode_exporter/gcode_export.py
@@ -5,22 +5,33 @@ import math
 
 
 class GCodeExporter(Module[tuple[Geometry, float], str]):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        gcode_comments: bool = True,
+        pretty_formatting: bool = True,
+        cut_speed: float = 20,
+        material_constant: float = 1.0,
+        dry_run: bool = True,
+        force_max_laser_power: bool = True,
+    ) -> None:
         super().__init__()
         self._gcode: str = ""
 
         # gcode settings
-        self.add_comments: bool = True
-        self.pretty_formatting: bool = True
+        self.gcode_comments: bool = gcode_comments
+        self.pretty_formatting: bool = pretty_formatting
 
-        # machine settings
-        self.cut_speed: float = 10  # mm/s
-        self.material_constant: float = 3.21  # s/mm^2
-        self.dry_run: bool = True
+        ### machine settings
+        # movement
+        self.cut_speed: float = cut_speed  # mm/s
+        # laser
+        self.material_constant: float = material_constant  # s/mm^2
+        self.dry_run: bool = dry_run
+        self.force_max_laser_power: bool = force_max_laser_power
 
     def _add_command(self, command: str, comment: str = ""):
         self._gcode += f"{command}"
-        if self.add_comments and comment != "":
+        if self.gcode_comments and comment != "":
             self._gcode += f"; {comment}"
         self._gcode += "\n"
 
@@ -28,22 +39,30 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
         rounded_value = round(value, precision)
 
         if self.pretty_formatting:
-            return f"{rounded_value:5.{precision}}"
+            return f"{rounded_value:4.{precision}f}"
         else:
             return f"{rounded_value:.{precision}}"
 
-    def discretize_cut_by_depth(
-        self, cut: TrapezoidalCut, max_deviation_mm: float = 0.5
-    ) -> list[TrapezoidalCut]:
-        return []
+    def calculate_laser_power(self, cut: TrapezoidalCut) -> float:
+        if self.force_max_laser_power:
+            return 1000
+
+        # TODO: this calculation is not that useful
+        start_depth: float = cut.start_segment().length()
+        end_depth: float = cut.end_segment().length()
+        max_depth: float = max(start_depth, end_depth)
+        return max_depth * self.cut_speed * self.material_constant
 
     def process(self, data: tuple[Geometry, float]) -> str:
         geometry, material_height = data
 
         self._add_command("G90", "absolute positioning")  # absolute positioning
         self._add_command("G21", "units in mm")  # use milimeters for XYZ
-        self._add_command("F6000", "feedrate")  # set feedrate
-        self._add_command("M8", "air assist on")  # air assist on (flood pin)
+        self._add_command(
+            f"F{self.format_float(self.cut_speed * 60)}", "feedrate"
+        )  # set feedrate (in mm/minute)
+        if not self.dry_run:
+            self._add_command("M8", "air assist on")  # air assist on (flood pin)
 
         last_config: None | Configuration = None
         for cut in geometry.cuts:
@@ -64,7 +83,7 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
 
             if not self.dry_run:
                 self._add_command(
-                    f"M4 S{self.format_float(cut.cut_depth * self.cut_speed * self.material_constant)}",
+                    f"M4 S{self.calculate_laser_power(cut)}",
                     "laser on",
                 )  # laser on dynamic power
 
@@ -72,10 +91,12 @@ class GCodeExporter(Module[tuple[Geometry, float], str]):
                 f"G1 X{self.format_float(end_config.x)} Y{self.format_float(end_config.y)} Z{self.format_float(material_height)} A{self.format_float(math.degrees(end_config.alpha) + 0.0)} B{self.format_float(math.degrees(end_config.beta) + 0.0)}",
                 "cut move",
             )  # cut
+
             if not self.dry_run:
                 self._add_command("M4 S0", "laser off")  # laser off
             last_config = end_config
 
-        self._add_command("M8.1", "air assist off")  # air assist off
+        if not self.dry_run:
+            self._add_command("M8.1", "air assist off")  # air assist off
         self._add_command("M2", "end")  # end of program
         return self._gcode
