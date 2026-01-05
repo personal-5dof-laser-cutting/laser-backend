ae3de7928e627115f0e80d89990ba24365e31dae
Edgar Bennemann <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-01-05 16:28:17 +0100

    improve test

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -1,8 +1,20 @@
 import requests
 import json
+import uvicorn
+import threading
+from time import sleep
+
+
+def start_backend():
+    from main import app
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 def test_unoptimized_pipeline():
+    threading.Thread(target=start_backend, daemon=True).start()
+    sleep(5)
+
     data = {
         "material_thickness": 10,
         "laser_off": True,
