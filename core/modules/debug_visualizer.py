890ab96ec7001cf6d9d15cc85eb85d287a19eb67
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-01-07 15:46:45 +0100

    display arrow for cut order inside the trapezoid and show travel moves

--- core/modules/debug_visualizer.py
+++ core/modules/debug_visualizer.py
@@ -1,69 +1,104 @@
-from core.models.geometry import Geometry, TrapezoidalCut
+from Geometry3D import Point
+from matplotlib.patches import PathPatch
+from matplotlib.textpath import TextPath
+from core.models.geometry import Configuration, Geometry, TrapezoidalCut
 from core.pipeline.base import Module
 
+from mpl_interactions import panhandler, zoom_factory
 import matplotlib.pyplot as plt
-from typing import Tuple
+
+import math
 from enum import IntFlag
 
 
-class Flags(IntFlag):
+class VisualizerFlags(IntFlag):
     SHOW_AREA = 0b01
     SHOW_ORDER = 0b10
 
 
-class DebugVisualizerModule(Module[Tuple[Geometry, Flags], Geometry]):
+class DebugVisualizerModule(Module[Geometry, Geometry]):
     max_x = 0
     max_y = 0
 
-    def process(self, data: Tuple[Geometry, Flags]) -> Geometry:
-        geometry, flags = data
-        shadeArea = bool(flags & Flags.SHOW_AREA)
-        showOrder = bool(flags & Flags.SHOW_ORDER)
+    def __init__(self, flags: VisualizerFlags) -> None:
+        super().__init__()
+
+        self.shadeArea = bool(flags & VisualizerFlags.SHOW_AREA)
+        self.showOrder = bool(flags & VisualizerFlags.SHOW_ORDER)
+
+    def process(self, data: Geometry) -> Geometry:
+        geometry = data
         self.fig, self.ax = plt.subplots()
-        for cut in geometry.cuts:
-            self._draw_cut(cut, shadeArea, showOrder)
+        for i, cut in enumerate(geometry.cuts):
+            self._draw_cut(cut, i)
+            if i != len(geometry.cuts) - 1:
+                if (
+                    geometry.cuts[i + 1].start_configuration()
+                    != cut.end_configuration()
+                ):
+                    self._draw_travel_move(
+                        cut.end_configuration(),
+                        geometry.cuts[i + 1].start_configuration(),
+                    )
         self.ax.set_xlim(-1, self.max_x + 5)
         self.ax.set_ylim(-1, self.max_y + 5)
         self.ax.set_aspect("equal")
+        zoom_factory(self.ax)
+        panhandler(self.fig)
 
         plt.show()
         return geometry
 
-    def _draw_cut(self, cut: TrapezoidalCut, shadeArea: bool, showOrder: bool):
+    def _draw_cut(self, cut: TrapezoidalCut, cut_number: int):
         start_top = cut.top_segment().start_point
         end_top = cut.top_segment().end_point
 
         start_bottom = cut.bottom_segment().start_point
         end_bottom = cut.bottom_segment().end_point
 
-        if showOrder:
-            self.ax.arrow(
-                start_top.x,
-                start_top.y,
-                end_top.x - start_top.x,
-                end_top.y - start_top.y,
-                length_includes_head=True,
-                color="red",
-                head_width=0.3,
-                head_length=0.3,
-            )
-            self.ax.arrow(
-                start_bottom.x,
-                start_bottom.y,
-                end_bottom.x - start_bottom.x,
-                end_bottom.y - start_bottom.y,
-                length_includes_head=True,
-                color="blue",
-            )
-        else:
-            self.ax.plot(
-                (start_top.x, end_top.x), (start_top.y, end_top.y), color="red"
-            )
+        is_slanted_cut = not math.isclose(cut.plane().n[2], 0)
+
+        self.ax.plot((start_top.x, end_top.x), (start_top.y, end_top.y), color="red")
+        if is_slanted_cut:
             self.ax.plot(
                 (start_bottom.x, end_bottom.x),
                 (start_bottom.y, end_bottom.y),
                 color="blue",
             )
+            if self.showOrder:
+                arrow_start = Point(
+                    (start_top.x + start_bottom.x) / 2,
+                    (start_top.y + start_bottom.y) / 2,
+                    0,
+                )
+                arrow_end = Point(
+                    (end_top.x + end_bottom.x) / 2, (end_top.y + end_bottom.y) / 2, 0
+                )
+                arrow_direction = Point(
+                    arrow_end.x - arrow_start.x, arrow_end.y - arrow_start.y, 0
+                )
+                self.ax.arrow(
+                    arrow_start.x + arrow_direction.x * 0.05,
+                    arrow_start.y + arrow_direction.y * 0.05,
+                    arrow_direction.x * 0.9,
+                    arrow_direction.y * 0.9,
+                    length_includes_head=True,
+                    color="black",
+                    width=0.1,
+                    head_width=1,
+                    head_length=1,
+                )
+
+                text_size = 2
+                text_path = TextPath(
+                    (
+                        (arrow_start.x + arrow_end.x) / 2 - text_size / 2,
+                        (arrow_start.y + arrow_end.y) / 2 - text_size / 2,
+                    ),
+                    str(cut_number),
+                    size=text_size,
+                )
+                self.ax.add_patch(PathPatch(text_path, color="black"))
 
         self.ax.plot(
             (start_top.x, start_bottom.x), (start_top.y, start_bottom.y), color="purple"
@@ -79,5 +114,13 @@ class DebugVisualizerModule(Module[Tuple[Geometry, Flags], Geometry]):
             [self.max_y, start_top.y, end_top.y, start_bottom.y, end_bottom.y]
         )
 
-    def _draw_travel_move(self):
-        pass
+    def _draw_travel_move(self, from_config: Configuration, to_config: Configuration):
+        self.ax.arrow(
+            from_config.x,
+            from_config.y,
+            to_config.x - from_config.x,
+            to_config.y - from_config.y,
+            length_includes_head=True,
+            color="gray",
+            width=0.075,
+        )
