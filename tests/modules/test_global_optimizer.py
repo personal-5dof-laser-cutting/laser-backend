d832d2a3f33f1ec13754ba153e84f2c3ef60322d
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-01-07 11:52:16 +0100

    remove old optimizer code and add new test

--- tests/modules/test_global_optimizer.py
+++ tests/modules/test_global_optimizer.py
@@ -1,9 +1,8 @@
-from math import inf
 import pytest
-import networkx as nx
-from Geometry3D import Point, Segment
+from Geometry3D import Point
 from core.modules.global_optimizer import GlobalOptimizerModule
-from core.models.geometry import Geometry, TrapezoidalCut, Configuration
+from core.modules.genetic_gtsp import GTSP, run_gcga
+from core.models.geometry import Geometry, TrapezoidalCut
 from core.service_container import Container
 from core.services.cost_function_service import (
     CostFunctionService,
@@ -18,188 +17,25 @@ SQUARE_PARAMS = [
 ]
 
 
-@pytest.mark.parametrize(
-    "cuts",
-    [
-        [
-            TrapezoidalCut(
-                Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-            ),
-            TrapezoidalCut(
-                Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-            ),
-        ],
-        [
-            TrapezoidalCut(
-                Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-            ),
-            TrapezoidalCut(
-                Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-            ),
-            TrapezoidalCut(
-                Point(3, 3, 0), Point(0, 0, 0), Point(2, 2, -5), Point(1, 1, -5)
-            ),
-        ],
-    ],
-)
-def test_build_graph(cuts: list[TrapezoidalCut]):
-    generated_graph = nx.Graph()
-    control_graph = nx.Graph()
-    cost_function = CostFunctionServiceImpl()
+@pytest.mark.parametrize("cuts", [SQUARE_PARAMS[:-1]])
+def test_tour_to_path(cuts: list[TrapezoidalCut]):
     optimizer = GlobalOptimizerModule()
-    optimizer._build_graph(generated_graph, cuts, cost_function, True)
-
-    expected_nodes = []
-    for cut in cuts:
-        expected_nodes += [cut] + cut.configurations()
-    control_graph.add_nodes_from(expected_nodes)
-    assert control_graph.nodes == generated_graph.nodes
-
-    for u in expected_nodes:
-        for v in expected_nodes:
-            if u != v:
-                control_graph.add_edge(u, v)
-
-    assert control_graph.edges == generated_graph.edges
-
-
-def test_triangle_constallation():
-    cuts = [
-        TrapezoidalCut(
-            Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-        ),
-        TrapezoidalCut(
-            Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-        ),
-        TrapezoidalCut(
-            Point(3, 3, 0), Point(0, 0, 0), Point(2, 2, -5), Point(1, 1, -5)
-        ),
-    ]
-    optimizer = GlobalOptimizerModule()
-    graph = nx.Graph()
-    optimizer._build_graph(graph, cuts, CostFunctionServiceImpl(), True)
-    assert (
-        graph.get_edge_data(cuts[0].start_configuration(), cuts[0].end_configuration())[
-            "weight"
-        ]
-        == inf
+    weights, groups = optimizer._generate_weights(cuts, Container.cost_function)
+    gtsp = GTSP(weights, groups)
+    best_chrom, _ = run_gcga(
+        gtsp,
+        pop_size=100,
+        generations=1000,
+        crossover_prob=0.9,
+        mutation_prob=0.15,
+        tournament_k=3,
+        elitism=2,
+        do_head_reopt=True,
     )
-    assert (
-        graph.get_edge_data(cuts[1].start_configuration(), cuts[1].end_configuration())[
-            "weight"
-        ]
-        == inf
-    )
-    assert (
-        graph.get_edge_data(cuts[2].start_configuration(), cuts[2].end_configuration())[
-            "weight"
-        ]
-        == inf
-    )
-    pass
-
-
-@pytest.mark.parametrize(
-    "path, cuts",
-    [
-        (
-            [
-                Configuration.from_segment(Segment(Point(0, 0, 0), Point(1, 1, -5))),
-                TrapezoidalCut(
-                    Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-                ),
-                Configuration.from_segment(Segment(Point(0, 3, 0), Point(1, 2, -5))),
-                Configuration.from_segment(Segment(Point(0, 3, 0), Point(1, 2, -5))),
-                TrapezoidalCut(
-                    Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-                ),
-                Configuration.from_segment(Segment(Point(3, 3, 0), Point(2, 2, -5))),
-                Configuration.from_segment(Segment(Point(3, 3, 0), Point(2, 2, -5))),
-                TrapezoidalCut(
-                    Point(3, 3, 0), Point(3, 0, 0), Point(2, 2, -5), Point(2, 1, -5)
-                ),
-                Configuration.from_segment(Segment(Point(3, 0, 0), Point(2, 1, -5))),
-                Configuration.from_segment(Segment(Point(3, 0, 0), Point(2, 1, -5))),
-                TrapezoidalCut(
-                    Point(3, 0, 0), Point(0, 0, 0), Point(2, 1, -5), Point(1, 1, -5)
-                ),
-                Configuration.from_segment(Segment(Point(0, 0, 0), Point(1, 1, -5))),
-            ],
-            [
-                TrapezoidalCut(
-                    Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-                ),
-                TrapezoidalCut(
-                    Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-                ),
-                TrapezoidalCut(
-                    Point(3, 3, 0), Point(3, 0, 0), Point(2, 2, -5), Point(2, 1, -5)
-                ),
-                TrapezoidalCut(
-                    Point(3, 0, 0), Point(0, 0, 0), Point(2, 1, -5), Point(1, 1, -5)
-                ),
-            ],
-        ),
-        (
-            [
-                Configuration.from_segment(Segment(Point(0, 0, 0), Point(1, 1, -5))),
-                TrapezoidalCut(
-                    Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-                ),
-                Configuration.from_segment(Segment(Point(0, 3, 0), Point(1, 2, -5))),
-                TrapezoidalCut(
-                    Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-                ),
-                Configuration.from_segment(Segment(Point(3, 3, 0), Point(2, 2, -5))),
-                TrapezoidalCut(
-                    Point(3, 3, 0), Point(3, 0, 0), Point(2, 2, -5), Point(2, 1, -5)
-                ),
-                Configuration.from_segment(Segment(Point(3, 0, 0), Point(2, 1, -5))),
-                TrapezoidalCut(
-                    Point(3, 0, 0), Point(0, 0, 0), Point(2, 1, -5), Point(1, 1, -5)
-                ),
-                Configuration.from_segment(Segment(Point(0, 0, 0), Point(1, 1, -5))),
-            ],
-            [
-                TrapezoidalCut(
-                    Point(0, 0, 0), Point(0, 3, 0), Point(1, 1, -5), Point(1, 2, -5)
-                ),
-                TrapezoidalCut(
-                    Point(0, 3, 0), Point(3, 3, 0), Point(1, 2, -5), Point(2, 2, -5)
-                ),
-                TrapezoidalCut(
-                    Point(3, 3, 0), Point(3, 0, 0), Point(2, 2, -5), Point(2, 1, -5)
-                ),
-                TrapezoidalCut(
-                    Point(3, 0, 0), Point(0, 0, 0), Point(2, 1, -5), Point(1, 1, -5)
-                ),
-            ],
-        ),
-    ],
-)
-def test_path_to_trapezoids(
-    path: list[TrapezoidalCut | Configuration], cuts: list[TrapezoidalCut]
-):
-    go = GlobalOptimizerModule()
-    ls_path = path.copy()
-    rs_path = path.copy()
-    trapezoids = go._path_to_trapezoids(path)
-    assert set(trapezoids) == set(cuts)
-
-    if len(path) == 0:
-        return
-
-    ls_path.pop()
-    ls_path.append(ls_path.pop(0))
-    ls_path.append(ls_path[0])
-    trapezoids = go._path_to_trapezoids(ls_path)
-    assert set(trapezoids) == set(cuts)
-
-    rs_path.pop()
-    rs_path.insert(0, rs_path.pop())
-    rs_path.append(rs_path[0])
-    trapezoids = go._path_to_trapezoids(rs_path)
-    assert set(trapezoids) == set(cuts)
+    tour = gtsp.decode(best_chrom)
+    trapezoid_path = optimizer._tour_to_path(tour, cuts, Container.cost_function)
+    for i in range(len(cuts)):
+        assert trapezoid_path[i] == cuts[i]
 
 
 @pytest.mark.parametrize("cuts", [SQUARE_PARAMS])
