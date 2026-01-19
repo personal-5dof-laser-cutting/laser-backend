132548b9f9b665e48138cd3f3fa8f288aebcf75d
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-01-19 12:22:15 +0100

    allow cross-origin resource sharing

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 from api.routers.base_router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def app_factory():
     api = FastAPI(title="Example FastAPI App")
+    origins = [
+        "http://127.0.0.1:8080",
+    ]
+    api.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     api.include_router(router)
     return api
 
 
-app = app_factory()
-
 if __name__ == "__main__":
+    app = app_factory()
     uvicorn.run(app, host="127.0.0.1", port=8000)
