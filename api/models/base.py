947bb4ae346ee31c679e30fa6ec77c3ec96ead1e
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-01-28 11:12:49 +0100

    update ,pdeös ised ny endpoints

--- api/models/base.py
+++ api/models/base.py
@@ -4,21 +4,15 @@ from pydantic import BaseModel, Field
 """Models used by endpoints. Descriptions provided will be visible in the docs UI (Swagger)."""
 
 
-class SomeInput(BaseModel):
-    message: str = Field(description="Provide docs here")
-
-
 class SvgToGcodeInput(BaseModel):
-    material_thickness: float
-    laser_off: bool
-    cut_speed: float
-    svg: str
-    optimize: bool
+    material_thickness: float = Field(description="Material thickness in mm")
+    laser_off: bool = Field(
+        description="If true, the laser will be disabled, but the path will still be walked"
+    )
+    cut_speed: float = Field(description="Speed of the laser cutter in mm/s")
+    svg: str = Field(description="Content of the svg file")
+    optimize: bool = Field(description="If true, the global optimizer will try")
 
 
 class GCodeOutput(BaseModel):
     gcode: Any = Field(description="Gcode")
-
-
-class SomeOutput(BaseModel):
-    message: str = Field(description="Provide docs here")
