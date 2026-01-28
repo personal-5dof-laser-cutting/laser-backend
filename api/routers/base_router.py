589b6ebec50881f870e30a8c150e690c6fd69697
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-01-28 11:21:43 +0100

    make cut_svg use a websocket to communicate with the laser cutter

--- api/routers/base_router.py
+++ api/routers/base_router.py
@@ -2,25 +2,10 @@ import io
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 
-from api.models.base import GCodeOutput, SomeOutput, SvgToGcodeInput
-from core.pipeline.pipeline import full_pipline_unoptimized, gcode_pipeline
+from api.models.base import SvgToGcodeInput
+from core.pipeline.pipeline import gcode_pipeline
 
 router = APIRouter()
-
-
-# sample get endpoint
-# will be available under http://127.0.0.1:8000/?inp=foo
-@router.get("/", response_model=SomeOutput)
-def test_endpoint_1(inp: str) -> SomeOutput:
-    return SomeOutput(message=inp)
-
-
-@router.post("/cut_svg", response_model=GCodeOutput)
-def svg_to_gcode(inp: SvgToGcodeInput) -> GCodeOutput:
-    pipeline = full_pipline_unoptimized(
-        inp.material_thickness, inp.laser_off, inp.cut_speed
-    )
-    return GCodeOutput(gcode=pipeline.run(inp.svg))
 
 
 @router.post(
