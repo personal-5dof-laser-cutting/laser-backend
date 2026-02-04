4b5171bc2454a78a8c888e92200fee0daa5d4599
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-04 12:11:17 +0100

    restructure cut_svg and add functionality accessing corgi via its static IP

--- api/routers/ws_router.py
+++ api/routers/ws_router.py
@@ -1,8 +1,9 @@
 import Geometry3D
 from fastapi import APIRouter, WebSocket
 
+from api.models.base import FrontendInput
 from core.corgi_interface import CorgiInterface
-from core.pipeline.pipeline import full_pipline_unoptimized
+from core.pipeline.pipeline import full_pipeline
 from gcode_lib.gcode_interface import GCodeInterface
 
 import threading
@@ -15,13 +16,9 @@ async def ws_cut_svg(ws: WebSocket):
     await ws.accept()
     Geometry3D.set_sig_figures(4)
     data = await ws.receive_json()
-    pipeline = full_pipline_unoptimized(
-        websocket=ws,
-        material_thickness=data["material_thickness"],
-        laser_off=data["laser_off"],
-        cut_speed_mm_per_s=data["cut_speed"],
-    )
+    frontendInput = FrontendInput(**data)
+    pipeline = full_pipeline(frontendInput)
     result: str = pipeline.run(data["svg"])
-    corgi_interface = CorgiInterface(GCodeInterface("192.168.0.1:81"))
+    corgi_interface = CorgiInterface(GCodeInterface("192.168.2.67:81"))
     threading.Thread(target=corgi_interface.main_loop, daemon=True).start()
     corgi_interface.send_lines(("$h\n" + result).split("\n"))
