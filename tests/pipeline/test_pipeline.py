293847406bccce2b8d59b3b2ece387e0d4c41da7
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-06 15:59:53 +0100

    remove unused function

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -1,14 +1,6 @@
-import uvicorn
-
 from api.models.base import FrontendInput
 from core.modules.global_optimizer.global_optimizer import GlobalOptimizerModule
 from core.pipeline.pipeline import full_pipeline
-
-
-def start_backend():
-    from main import app
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 def test_full_pipeline():
