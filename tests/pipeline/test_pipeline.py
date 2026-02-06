320c82a0a750c16d393e4509389287615c875eae
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-06 14:46:31 +0100

    adapt pipeline test
    tihs probably needs to get revamped once all features are added

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -1,8 +1,7 @@
-import requests
-import json
 import uvicorn
-import threading
-from time import sleep
+
+from api.models.base import FrontendInput
+from core.pipeline.pipeline import full_pipeline
 
 
 def start_backend():
@@ -11,15 +10,18 @@ def start_backend():
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
-def test_unoptimized_pipeline():
-    threading.Thread(target=start_backend, daemon=True).start()
-    sleep(5)
+def test_full_pipeline():
+    input = FrontendInput(
+        material="wood",
+        material_thickness=10,
+        cut_speed=5,
+        laser_off=True,
+        optimize=False,
+        svg=open("tests/svg5dof/svgs/circle_2.svg").read(),
+        scaling="mm",
+    )
 
-    data = {
-        "material_thickness": 10,
-        "laser_off": True,
-        "cut_speed": 10,
-        "svg": open("tests/svg5dof/svgs/circle_2.svg").read(),
-    }
-    r = requests.post("http://127.0.0.1:8000/post", json.dumps(data))
-    assert r.status_code == 200
+    pipeline = full_pipeline(input)
+    # TODO: Assert that no optimizer module is in pipeline.modules
+    g_code = pipeline.run(input.svg)
+    assert g_code
