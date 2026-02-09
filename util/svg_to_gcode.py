8311df6b295855dbcaa86fd950474ddc50a5e2b2
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-02-09 15:10:01 +0100

    fix typo in utils script

--- util/svg_to_gcode.py
+++ util/svg_to_gcode.py
@@ -1,8 +1,10 @@
 from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
 from core.modules.gcode_exporter.gcode_export import GCodeExporter
 import sys
+from Geometry3D import set_sig_figures
 
 if __name__ == "__main__":
+    set_sig_figures(4)
     if len(sys.argv) != 6:
         print("Usage: material_thickness scaling cut_speed svg_file output_file")
         sys.exit(1)
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     svg_path: str = sys.argv[4]
     gcode_output: str = sys.argv[5]
 
-    if scaling not in ["mm", "illsturator"]:
+    if scaling not in ["mm", "illustrator"]:
         raise Exception("Invalid scaling")
     importer = SVG5DOF_Importer(material_thickness=material_thickness, scaling=scaling)  # type: ignore
     geo = importer.process(open(svg_path, "r").read())
