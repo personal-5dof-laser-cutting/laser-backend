f47de0518dd8f56d09a5dfba7a00421a9bfaba0f
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-16 13:50:37 +0100

    add default values to x_offset and y_offset

--- api/models/base.py
+++ api/models/base.py
@@ -15,18 +15,20 @@ class FrontendInput(BaseModel):
     material_thickness: float = Field(description="Material thickness in mm")
     cut_speed: float = Field(description="Speed of the laser cutter in mm/s")
     laser_off: bool = Field(
-        description="If true, the laser will be disabled, but the path will still be walked"
+        description="If true, the laser will be disabled, but the path will still be walked",
     )
     optimize: bool = Field(
-        description="If true, the global optimizer will approximate an optimized cut order"
+        description="If true, the global optimizer will approximate an optimized cut order",
     )
     svg: str = Field(description="Content of the svg file")
     scaling: ScalingType = Field(
-        description="Indicates if SVG was created with Adobe Illustrator scaling or has a scaling in mm"
+        description="Indicates if SVG was created with Adobe Illustrator scaling or has a scaling in mm",
     )
     x_offset: int = Field(
-        description="How many millimeters the svg should be moved alongside the x-axis"
+        description="How many millimeters the svg should be moved alongside the x-axis",
+        default=0,
     )
     y_offset: int = Field(
-        description="How many millimeters the svg should be moved alongside the y-axis"
+        description="How many millimeters the svg should be moved alongside the y-axis",
+        default=0,
     )
