d57763eac09795b09e68f5e992721473faa6ac6d
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-02-16 15:21:31 +0100

    temp fix for svg5dof design error

--- core/modules/svg5dof_importer/import_svg5dof.py
+++ core/modules/svg5dof_importer/import_svg5dof.py
@@ -182,7 +182,15 @@ class SVG5DOF_Importer(Module[str, Geometry]):
         for element in svg_root:
             tag = element.tag.split(svg_namespace)[-1]
             match tag:
-                case "line" | "path" | "rect" | "circle" | "polygon" | "polyline":
+                case (
+                    "line"
+                    | "path"
+                    | "rect"
+                    | "circle"
+                    | "polygon"
+                    | "polyline"
+                    | "ellipse"
+                ):
                     paths, _ = svg.svgstr2paths(  # type: ignore
                         ET.tostring(element, encoding="unicode")
                     )
@@ -197,6 +205,9 @@ class SVG5DOF_Importer(Module[str, Geometry]):
                     cut_depth: float = (
                         self._5dof_color_to_percentage(color) * self.material_thickness
                     )
+
+                    if math.isclose(cut_depth, 0):
+                        cut_depth = self.material_thickness
 
                     cuts = points_to_trapezoids(
                         self._scale_points(bottom_points),
@@ -230,6 +241,9 @@ class SVG5DOF_Importer(Module[str, Geometry]):
                     bottom_points, top_points = svg_paths_to_points(
                         bottom_path[0], top_path[0]
                     )
+
+                    if len(bottom_points) == 0 and len(top_points) == 0:
+                        continue
 
                     bottom_color = svg_color_to_rgba(bottom_element.attrib["stroke"])
                     cut_depth: float = (
