3bf78cb327b34f0a530f34b1e14fc25121938b17
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-02-17 10:03:32 +0100

    laser power calibration script

--- util/calibrate_laser_power.py
+++ util/calibrate_laser_power.py
@@ -3,7 +3,6 @@ from core.corgi_interface import CorgiInterface
 from gcode_lib.gcode_interface import GCodeInterface
 import sys
 import threading
-from mock import Mock
 
 MAX_POWER = 1000
 interface: CorgiInterface
@@ -14,8 +13,8 @@ def home():
     interface.send_line("$h")
 
 
-def move(x: float, y: float):
-    interface.send_line(f"G0 X{x:z.3f} Y{y:z.3f} Z{material_height} A0 B0")
+def move(x: float, y: float, a: float = 0, b: float = 0):
+    interface.send_line(f"G0 X{x:z.3f} Y{y:z.3f} Z{material_height} A{a} B{b}")
 
 
 def laser_on(laser_power: float):
@@ -32,8 +31,8 @@ def set_feedrate(feedrate: float):
     interface.send_line(f"F{feedrate:z.2f}")
 
 
-def cut(x: float, y: float):
-    interface.send_line(f"G1 X{x} Y{y} Z{material_height} A0 B0")
+def cut(x: float, y: float, a: float = 0, b: float = 0):
+    interface.send_line(f"G1 X{x} Y{y} Z{material_height} A{a} B{b}")
 
 
 def cut_square(x, y, width=7, height=7):
@@ -60,27 +59,38 @@ def grid():
             laser_off()
 
 
-if __name__ == "__main__":
-    address = sys.argv[1]
-    material_height = float(sys.argv[2])
-
-    interface = CorgiInterface(GCodeInterface(address))
-    threading.Thread(target=interface.main_loop, daemon=True).start()
-    # interface = Mock()
-
-    if input("Needs homing? y/n") == "y":
-        home()
-
-    if input("Go to x=100 y= 100? y/n") == "y":
+def line():
+    for xi, i in enumerate(range(0, 257, 16)):
+        if i == 0:
+            continue
+        power = i
+        speed = 5
+        x = xi * 6 + 100
+        y = 100
+        print(f"Power {power}, speed {speed} mm/s x={x} y={y}")
+        set_feedrate(speed * 60)
+        laser_on(power)
+        cut_square(x, y, 5, 5)
         laser_off()
-        move(100, 100)
 
-    input("Start? Any key!")
 
-    # grid()
+def cut_v_groove(x: float, y: float, angle_deg: float, width: float, depth: float):
+    offset: float = math.tan(math.radians(angle_deg)) * depth
 
-    # sys.exit(0)
+    move(x - offset, y, 0, angle_deg)
+    cut(x - offset, y + width, 0, angle_deg)
 
+    move(x + offset, y + width, 0, -angle_deg)
+    cut(x + offset, y, 0, -angle_deg)
+
+    move(x - offset, y + width)
+    cut(x + offset, y + width)
+
+    move(x - offset, y)
+    cut(x + offset, y)
+
+
+def binary_search():
     print("Table Calibration Script")
     speed_mm_per_s = 6
     set_feedrate(speed_mm_per_s * 60)  # 10 mm/s
@@ -111,3 +121,43 @@ if __name__ == "__main__":
 
     print(f"Material constant {upper / speed_mm_per_s / material_height}")
     print("Done")
+
+
+if __name__ == "__main__":
+    address = sys.argv[1]
+    material_height = float(sys.argv[2])
+
+    interface = CorgiInterface(GCodeInterface(address))
+    threading.Thread(target=interface.main_loop, daemon=True).start()
+    # interface = Mock()
+
+    if input("Needs homing? y/n") == "y":
+        home()
+
+    set_feedrate(240)
+
+    x = 100
+    y = 100
+
+    if input(f"Go to x={x} y={y}? y/n") == "y":
+        laser_off()
+        move(x, y)
+
+    for angle in range(10, 55, 5):
+        print("\n#######################")
+        print(f"New Angle {angle}°\n")
+        lower = 1
+        upper = 255
+        while lower < upper:
+            mid = math.floor((lower + upper) / 2)
+            print(f"Cutting with laser power {mid} (0-255)")
+            laser_on(mid)
+            cut_v_groove(x, y, angle, 10, 3)
+            laser_off()
+            x += 10
+            if input("Did it cut through? y/n: ").strip() == "y":
+                upper = mid
+            else:
+                lower = mid + 1
+
+    input("Start? Any key!")
