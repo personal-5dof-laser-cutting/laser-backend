f9ad082b58097a9d8f9cb452ce843385a827035b
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-19 15:55:00 +0100

    improve modularity and precision
    added the bucket epsilon as a class attribute and changed unneccessary floats into ints

--- core/modules/bucket_optimizer/bucket_optimizer.py
+++ core/modules/bucket_optimizer/bucket_optimizer.py
@@ -4,24 +4,28 @@ from core.pipeline.base import Module
 
 
 class BucketOptimizerModule(Module[Geometry, Geometry]):
+    def __init__(self, epsilon: float = 0.01) -> None:
+        self.epsilon = epsilon
+        super().__init__()
+
     def process(self, data: Geometry) -> Geometry:
         optimized_geo = Geometry()
-        sorted_cuts = sorted(data.cuts, key=lambda cut: self._get_sort_tuple(cut))
+        sorted_cuts = sorted(data.cuts, key=self._get_sort_tuple)
         optimized_geo.add_cuts(sorted_cuts)
         return optimized_geo
 
-    def _get_sort_tuple(self, cut: TrapezoidalCut) -> tuple[float, float, float, float]:
+    def _get_sort_tuple(self, cut: TrapezoidalCut) -> tuple[int, int, int, int]:
         table_angle, laser_head_angle = cut.cutter_angles(unit="radian")
         # We choose only configuration options with positive table angle for better bucketing
         if table_angle < 0 and not math.isclose(table_angle, 0):
             table_angle += math.pi
             laser_head_angle *= -1
-        epsilon = 0.01
-        table_angle = self.round_to_epsilon(table_angle, epsilon)
-        laser_head_angle = self.round_to_epsilon(laser_head_angle, epsilon)
-        x_pos = self.round_to_epsilon(cut.start_configuration().x, epsilon)
-        y_pos = self.round_to_epsilon(cut.start_configuration().y, epsilon)
-        return (table_angle, laser_head_angle, x_pos, y_pos)
+        table_angle_bucket = self.get_bucket_index(table_angle)
+        laser_head_angle_bucket = self.get_bucket_index(laser_head_angle)
+        start_config = cut.start_configuration()
+        x_pos_bucket = self.get_bucket_index(start_config.x)
+        y_pos_bucket = self.get_bucket_index(start_config.y)
+        return (table_angle_bucket, laser_head_angle_bucket, x_pos_bucket, y_pos_bucket)
 
-    def round_to_epsilon(self, value: float, epsilon: float) -> float:
-        return round(value / epsilon) * epsilon
+    def get_bucket_index(self, value: float) -> int:
+        return round(value / self.epsilon)
