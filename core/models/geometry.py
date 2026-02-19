31ea26c8d7a054f7b9d2edec1d40d9a098ff69d5
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-02-19 17:27:21 +0100

    improve legibility

--- core/models/geometry.py
+++ core/models/geometry.py
@@ -256,6 +256,9 @@ class TrapezoidalCut:
         if math.isclose(start_direction[0], 0) and math.isclose(start_direction[1], 0):
             return (0, 0)
 
+        def float_gt(greater: float, than: float) -> bool:
+            return greater > than and not math.isclose(greater, than)
+
         cut_direction: Vector = self.top_vector()
         cut_direction_2D: Vector = Vector(cut_direction[0], cut_direction[1], 0)
         table_angle = cut_direction_2D.angle(y_unit_vector())
@@ -264,17 +267,15 @@ class TrapezoidalCut:
             table_angle -= math.pi
 
         # We check if it cuts towards negative x to see if the table will rotated clockwise (negative angle)
-        if (
-            cut_direction_2D[0] < 0
-            and not math.isclose(cut_direction_2D[0], 0)
-            and not math.isclose(table_angle, math.pi / 2)
+        if float_gt(0, cut_direction_2D[0]) and not math.isclose(
+            table_angle, math.pi / 2
         ):
             table_angle *= -1
 
         angle_compare_vector = Vector(start_direction[0], start_direction[1], 0)
         # If the cut is slanted towards +x or only slanted towards -y (in which case the table rotates 90° counter clockwise and the head points towards +x),
         # we flip the angle compare vector to get an angle between -90° and 90°
-        if (start_direction[0] > 0 and not math.isclose(start_direction[0], 0)) or (
+        if float_gt(start_direction[0], 0) or (
             math.isclose(start_direction[0], 0) and start_direction[1] < 0
         ):
             angle_compare_vector = -angle_compare_vector
