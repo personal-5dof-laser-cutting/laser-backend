1846cccfc3928b7a5b2e0cd2c57bfc13b9f97371
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-02-23 13:12:54 +0100

    use scaling type for util script

--- util/svg_to_gcode.py
+++ util/svg_to_gcode.py
@@ -1,5 +1,6 @@
 from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
 from core.modules.gcode_exporter.gcode_export import GCodeExporter
+from api.models.base import ScalingType
 import sys
 from Geometry3D import set_sig_figures
 
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     svg_path: str = sys.argv[5]
     gcode_output: str = sys.argv[6]
 
-    if scaling not in ["mm", "illustrator"]:
+    if scaling not in ScalingType:
         raise Exception("Invalid scaling")
     importer = SVG5DOF_Importer(material_thickness=material_thickness, scaling=scaling)  # type: ignore
     geo = importer.process(open(svg_path, "r").read())
