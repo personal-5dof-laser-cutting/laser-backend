244efa80846da7efaebae7575f95f52815f42fad
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-03-02 15:43:51 +0100

    use pytest mock for corgi interface test

--- tests/corgi_interface/test_interface.py
+++ tests/corgi_interface/test_interface.py
@@ -1,47 +1,30 @@
-from core.corgi_interface import CorgiInterface, str_len
+from gcode_lib.gcode_interface import GCodeInterface
+
+from core.corgi_interface import CorgiInterface
 import threading
-from time import sleep
+from time import time, sleep
+from pytest_mock import MockerFixture
 
 BUFFER_SIZE = 10
 
 
-class GCodeInterface:
-    def __init__(self, address: str):
-        self.buffer_used: int = 0
-        self.buffer: list[str] = []
+def test_buffer(mocker: MockerFixture):
+    mock_interface = mocker.Mock(spec=GCodeInterface)
+    mock_interface.recv.return_value = "ok"
+    corgi_interface = CorgiInterface(mock_interface, buffer_size=BUFFER_SIZE)
 
-        self.time_left: int = 10
-
-    def open(self) -> None:
-        return
-
-    def send(self, msg: str) -> None:
-        assert msg.endswith("\n")
-
-        byte_count: int = str_len(msg)
-        self.buffer_used += byte_count
-        self.buffer.append(msg)
-
-        print(f"sent {msg}, buffer_used: {self.buffer_used}")
-
-        assert self.buffer_used <= BUFFER_SIZE
-
-    def recv(self) -> None | str:
-        if self.time_left > 0:
-            self.time_left -= 1
-            return None
-        elif len(self.buffer) > 0:
-            self.time_left = 10
-            msg = self.buffer.pop(0)
-            self.buffer_used -= str_len(msg)
-            return "ok"
-
-
-def test_buffer():
-    interface = CorgiInterface(GCodeInterface("asdasd"), buffer_size=BUFFER_SIZE)
-    t = threading.Thread(target=interface.main_loop, daemon=True)
+    t = threading.Thread(target=corgi_interface.main_loop, daemon=True)
     t.start()
 
-    interface.send_lines(["12345", "12345", "12", "12345", "123124214"])
-    sleep(1)
-    assert len(interface.queue) == 0
+    lines = ["12345", "12345", "12", "12345", "123124214"]
+    corgi_interface.send_lines(lines)
+
+    start_time = time()
+    while len(corgi_interface.queue) > 0 and time() - start_time < 20:
+        sleep(0.01)
+    assert len(corgi_interface.queue) == 0
+    mock_interface.open.assert_called_once()
+    assert mock_interface.send.call_count == len(lines)
+    for line in lines:
+        mock_interface.send.assert_any_call(line + "\n")
+    mock_interface.recv.assert_called()
