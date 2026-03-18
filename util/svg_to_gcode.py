db344a29b1466266856ff1d684184dcb517e2c8e
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-03-18 11:34:43 +0100

    remove unused imports

--- util/svg_to_gcode.py
+++ util/svg_to_gcode.py
@@ -1,9 +1,6 @@
 from core.modules.auto_nester.auto_nester import AutoNester
 from core.modules.bucket_optimizer.bucket_optimizer import BucketOptimizerModule
 from core.modules.debug_visualizer.debug_visualizer import DebugVisualizerModule
-from core.modules.geometry_visualizer.geometry_visualizer import (
-    GeometryVisualizerModule,
-)
 from core.modules.local_optimizer.local_optimizer import LocalOptimizer
 from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
 from core.modules.gcode_exporter.gcode_export import GCodeExporter
