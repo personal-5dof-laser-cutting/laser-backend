810556fb902030407ddb9cf9936eff6386ca5373
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-05-13 18:09:04 +0200

    remove unneccessary recalculation of previous_cost in _two_opt

--- core/modules/greedy_optimizer/greedy_optimizer.py
+++ core/modules/greedy_optimizer/greedy_optimizer.py
@@ -105,11 +105,11 @@ class GreedyOptimizerModule(Module[Geometry, Geometry]):
         cuts = self.geometry.cuts
         found_improvement: bool = True
         iterations: int = 0
+        previous_cost: float = self.geometry.calculate_travel_cost(
+            self.material_height, True
+        )
+        improvement: float = 0.0
         while found_improvement and iterations < self.max_iterations:
-            previous_cost: float = self.geometry.calculate_travel_cost(
-                self.material_height, True
-            )
-            improvement: float = 0.0
             found_improvement = False
             for cut1, cut2 in combinations(range(len(cuts)), 2):
                 flip_1, flip_2, flip_delta = self._calculate_flip_improvement(
