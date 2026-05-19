3606fa7fe9b071ae7b1de67af5bce0a6c9ce91a2
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-05-19 10:57:05 +0200

    add missing parameter to calculate_travel_cost()

--- tests/modules/test_genetic_optimizer.py
+++ tests/modules/test_genetic_optimizer.py
@@ -30,10 +30,12 @@ def test_genetic_optimizer(cuts: list[TrapezoidalCut]):
 
     material_height = 5
     optimizer = GeneticOptimizerModule(material_height)
-    original_costs = geo_rep.calculate_travel_cost(material_height)
+    original_costs = geo_rep.calculate_travel_cost(material_height, as_tour=False)
 
     optimized_geo = optimizer.process(geo_rep)
-    optimized_costs = optimized_geo.calculate_travel_cost(material_height)
+    optimized_costs = optimized_geo.calculate_travel_cost(
+        material_height, as_tour=False
+    )
 
     assert original_costs > optimized_costs or original_costs == pytest.approx(
         optimized_costs
