c3d3d362f7a1db2b6ca1151043c830054cab25e9
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-06-01 16:36:55 +0200

    add util script for running optimizer

--- tests/modules/test_greedy_optimizer.py
+++ tests/modules/test_greedy_optimizer.py
@@ -1,6 +1,7 @@
 import Geometry3D
 import pytest
 from core.models.geometry import Geometry
+from core.modules.auto_nester.auto_nester import AutoNester
 from core.modules.greedy_optimizer.greedy_optimizer import GreedyOptimizerModule
 from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
 
@@ -12,6 +13,10 @@ from core.modules.svg5dof_importer.import_svg5dof import SVG5DOF_Importer
             open("tests/modules/svgs/my-guy-wip.svg").read(),
             6,
         ),
+        (
+            open("tests/modules/svgs/the-guy-new.svg").read(),
+            6,
+        ),
     ],
 )
 def test_optimizer(svg_string: str, material_height: float):
@@ -19,8 +24,10 @@ def test_optimizer(svg_string: str, material_height: float):
     Geometry3D.set_sig_figures(4)
     dof = SVG5DOF_Importer(material_height, 72)
     geometry: Geometry = dof.process(svg_string)
-    opt = GreedyOptimizerModule(material_height)
+    an = AutoNester(200, 200)
+    geometry = an.process(geometry)
+    opt = GreedyOptimizerModule(material_height, show_statistics=False)
     previous_costs = geometry.calculate_travel_cost(material_height, False)
     optimized = opt.process(geometry)
     optimized_costs = optimized.calculate_travel_cost(material_height, False)
-    assert previous_costs >= optimized_costs
+    assert not previous_costs < optimized_costs
