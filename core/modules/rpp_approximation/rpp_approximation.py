3ee93091df06365f6099c337f64c386bf224577b
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-06-16 15:58:48 +0200

    improve type safety and efficiency

--- core/modules/rpp_approximation/rpp_approximation.py
+++ core/modules/rpp_approximation/rpp_approximation.py
@@ -1,49 +1,54 @@
 from itertools import combinations, product
+from typing import Literal
 
 import networkx as nx
+from pydantic import BaseModel
 
 from core.models.geometry import Configuration, Geometry, TrapezoidalCut
-from core.pipeline.base import Module
+from core.modules.base_optimizer import BaseOptimizer
 from core.service_container import Container
-from core.services.kinematics_service import KinematicsService
 
 
-class RPPApproximationModule(Module[Geometry, Geometry]):
+class CutEdgeData(BaseModel):
+    is_cut_move: Literal[True] = True
+    original_edge_index: int
+
+
+class RPPApproximationModule(BaseOptimizer):
     def __init__(self, material_height: float) -> None:
-        super().__init__()
-        self.material_height: float = material_height
-        self.kinematics: KinematicsService = Container.kinematics_service
+        super().__init__(material_height)
 
-    def process(self, data: Geometry) -> Geometry:
-        self.geometry: Geometry = data
+    def get_current_cost(self, as_cycle: bool) -> float:
+        return self.geometry.calculate_travel_cost(self.material_height, as_cycle)
+
+    def _optimize(self):
+        self.original_cuts: list[TrapezoidalCut] = self.geometry.cuts
         self._build_cache()
-        graph: nx.Graph = self._build_graph()
+        graph: nx.Graph[Configuration] = self._build_graph()
         self._connect_graph_minimally(graph)
         multi_graph: nx.MultiGraph = self._add_mwpm_edges(graph)
-        return self._graph_to_geometry(multi_graph)
+        self.geometry = self._graph_to_geometry(multi_graph)
 
     def _build_cache(self):
         cuts: list[TrapezoidalCut] = self.geometry.cuts
         configurations: set[Configuration] = {
             config for cut in cuts for config in cut.configurations()
         }
-        self.kinematics.generate_cache(list(configurations), self.material_height)
+        Container.kinematics_service.generate_cache(
+            configurations, self.material_height
+        )
 
-    def _build_graph(self) -> nx.Graph:
-        graph: nx.Graph = nx.Graph()
-        for cut in self.geometry.cuts:
+    def _build_graph(self) -> nx.Graph[Configuration]:
+        graph: nx.Graph[Configuration] = nx.Graph()
+        for index, cut in enumerate(self.geometry.cuts):
             start_conf, end_conf = cut.configurations()
             graph.add_edge(
-                start_conf,
-                end_conf,
-                weight=start_conf.travel_time_to(end_conf, self.material_height),
-                depth=cut.cut_depth,
-                is_cut_move=True,
+                start_conf, end_conf, is_cut_move=True, original_edge_index=index
             )
         return graph
 
-    def _connect_graph_minimally(self, graph: nx.Graph):
-        component_graph: nx.Graph | None = self._get_component_graph(graph)
+    def _connect_graph_minimally(self, graph: nx.Graph[Configuration]):
+        component_graph: nx.Graph[int] | None = self._get_component_graph(graph)
         if component_graph is None:
             return
         for _, _, data in nx.minimum_spanning_edges(
@@ -51,14 +56,15 @@ class RPPApproximationModule(Module[Geometry, Geometry]):
         ):
             actual_from = data["actual_from"]
             actual_to = data["actual_to"]
-            graph.add_edge(
-                actual_from, actual_to, weight=data["weight"], is_cut_move=False
-            )
+            weight = data["weight"]
+            graph.add_edge(actual_from, actual_to, weight=weight, is_cut_move=False)
 
-    def _get_component_graph(self, graph: nx.Graph) -> nx.Graph | None:
+    def _get_component_graph(
+        self, graph: nx.Graph[Configuration]
+    ) -> nx.Graph[int] | None:
         if nx.is_connected(graph):
             return None
-        component_graph = nx.Graph()
+        component_graph: nx.Graph[int] = nx.Graph()
         self.components: list[set[Configuration]] = [
             component for component in nx.connected_components(graph)
         ]
@@ -92,41 +98,38 @@ class RPPApproximationModule(Module[Geometry, Geometry]):
                 to_conf = conf_b
         return min_distance, from_conf, to_conf
 
-    def _add_mwpm_edges(self, graph: nx.Graph) -> nx.MultiGraph:
-        matching_graph: nx.Graph = nx.Graph()
+    def _add_mwpm_edges(
+        self, graph: nx.Graph[Configuration]
+    ) -> nx.MultiGraph[Configuration]:
+        matching_graph: nx.Graph[Configuration] = nx.Graph()
         odd_nodes = self._get_odd_degree_nodes(graph)
         for u, v in combinations(odd_nodes, 2):
             matching_graph.add_edge(
                 u, v, weight=u.travel_time_to(v, self.material_height)
             )
         matching = nx.min_weight_matching(matching_graph, weight="weight")
-        multi_graph = nx.MultiGraph(graph)
+        multi_graph: nx.MultiGraph[Configuration] = nx.MultiGraph(graph)
         for u, v in matching:
+            weight = matching_graph.get_edge_data(u, v)["weight"]
             multi_graph.add_edge(
                 u,
                 v,
-                weight=u.travel_time_to(v, self.material_height),
+                weight=weight,
                 is_cut_move=False,
             )
         return multi_graph
-        # pos = {node: (node.x*2, node.y*2) for node in self.graph.nodes()}
-        # nx.draw(self.graph, pos)
-        # pos = {node: (node.x*2, node.y*2) for node in debug_graph.nodes()}
-        # nx.draw(debug_graph, pos, node_color="#3dd90054", edge_color="red")
-        # plt.show()
 
-    def _get_odd_degree_nodes(self, graph: nx.Graph) -> list[Configuration]:
+    def _get_odd_degree_nodes(
+        self, graph: nx.Graph[Configuration]
+    ) -> list[Configuration]:
         return [conf for conf, degree in graph.degree() if degree % 2 == 1]
 
     def _graph_to_geometry(self, graph: nx.MultiGraph) -> Geometry:
-        # print("draww")
-        # nx.draw(self.graph)
-        # plt.show()
-        # sleep(60)
         geometry = Geometry()
-        for u, v in nx.eulerian_circuit(graph):
-            data = graph.get_edge_data(u, v)
-            if data and data[0]["is_cut_move"]:
-                geometry.add_cut_from_configurations(u, v, data[0]["depth"])
+        for u, v, key in nx.eulerian_circuit(graph, keys=True):
+            data = graph.get_edge_data(u, v)[key]
+            if data.get("is_cut_move"):
+                edge = CutEdgeData.model_validate(data)
+                geometry.add_cut(self.original_cuts[edge.original_edge_index])
         geometry.shift_path_optimally(self.material_height)
         return geometry
