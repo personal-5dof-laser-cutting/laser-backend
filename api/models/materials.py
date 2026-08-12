e14809701d4081c48f3f5d971725753e576118ea
Leonard del Federico <leonard.delfederico@student.hpi.uni-potsdam.de>
2026-08-12 15:42:26 +0200

    add laser_power to FrontendInput and suggested_cutspeed to materials

--- api/models/materials.py
+++ api/models/materials.py
@@ -4,10 +4,11 @@ from pydantic import BaseModel, Field
 class MaterialData(BaseModel):
     id: str = Field(description="Material ID")
     label: str = Field(description="Material name / label")
+    suggested_cutspeed: float = Field(description="Suggested cutspeed for the material")
     standard_thickness: float = Field(description="Standard thickness", default=5.0)
 
 
 MATERIALS: list[MaterialData] = [
-    MaterialData(id="wood", label="wood", standard_thickness=5),
-    MaterialData(id="mdf", label="MDF", standard_thickness=5),
+    MaterialData(id="wood", label="wood", suggested_cutspeed=20, standard_thickness=5),
+    MaterialData(id="mdf", label="MDF", suggested_cutspeed=30, standard_thickness=5),
 ]
